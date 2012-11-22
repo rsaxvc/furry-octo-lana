451a0e9dfa2b16c7d2100185123b594dad8aeb88
Richard Allen <rsaxvc@gmail.com>
2012-11-22 09:33:37 -0600

    Remove printout

--- entity.cpp
+++ entity.cpp
@@ -15,18 +15,11 @@ float entity::getSpeed()
 return sqrt( vel.dx*vel.dx + vel.dy*vel.dy );
 }
 
-#include <iostream>
-
 void entity::calcState()
 {
 last_pos = pos;
 
 pos.x += vel.dx * PHYSICS_TIMESTEP;
 pos.y += vel.dy * PHYSICS_TIMESTEP;
-
-if( getClass() == CLASS_BULLET )
-	{
-	std::cout<<"pos:("<<pos.x<<","<<pos.y<<")\tlast_pos("<<last_pos.x<<","<<last_pos.y<<")"<<std::endl;
-	}
 }
 
